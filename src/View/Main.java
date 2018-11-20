b4fda303e5feec1e85c7f92643be25f2f62f8023
Almir Neto <almirneto338@gmail.com>
2018-11-20 01:53:16 -0300

    Syntatic added

--- src/View/Main.java
+++ src/View/Main.java
@@ -1,18 +1,16 @@
 package View;
 
-import Model.LexicalAnalyzer;
-import Model.Token;
+import Model.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 public class Main {
 
-    private static LexicalAnalyzer analyzer = new LexicalAnalyzer();
+    private static final LexicalAnalyzer lexical = new LexicalAnalyzer();
+    private static final SyntaticAnalyzer syntatic = new SyntaticAnalyzer();
 
     public static void main(String[] args) throws IOException {
-
         /* Try to open 'teste' directory.
         If it doesn't exists, stops execution.*/
         File dir = new File("teste");
@@ -22,16 +20,13 @@ public class Main {
             /* Run the algorithm for all '.txt' in teste directory*/
             for (File files : dir.listFiles()) {
                 if (files.getName().endsWith(".txt")) {
-                    ArrayList<ArrayList<Token>> ret = analyzer.start(files);
+                    ArrayList<ArrayList<Token>> ret = lexical.start(files);
                     ArrayList<Token> tokens = ret.get(0);
                     ArrayList<Token> errors = ret.get(1);
-                    
-                    analyzer.writeDataOut(files.getName());
 
-                    //System.out.println(ret);
+                    lexical.writeDataOut(files.getName());
                 }
             }
         }
     }
-
 }
